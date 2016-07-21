ac052e63cd35001ca781b616c5f6d1d249a12c60
Fly Man <fly.man.opensim@gmail.com>
2016-07-21 10:13:24 +0200

    Installation for Ubuntu
    Changed MySQL to MariaDB

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
 sudo apt-get install mono-complete
-# Install MySQL Server
-sudo apt-get install mysql-server
+# Install MariaDB Server
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mariadb.mirror.triple-it.nl/repo/10.1/ubuntu xenial main'
+sudo apt update
+sudo apt-get install mariadb-server
 # Install Git
 sudo apt-get install git-core
 cd /
