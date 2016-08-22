09428b4ec51676f1d90414b8d39ea26b026328df
WhiteCoreWebUI <nightbeatz89@gmx.net>
2016-08-22 21:29:20 +0200

    Change Backup Path

--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 # Check for any updates of Linux
 sudo apt-get update
-# go to /
-cd /
-# Run backup of current Working WC System
-tar czf backup.$(date +%Y%m%d-%H%M%S).tar.gz Github
 # Update WhiteCore-Sim
-cd Github/
+cd /Github/
+# Run backup of current Working WC System
+tar czf backup.$(date +%Y%m%d-%H%M%S).tar.gz WhiteCore-Dev
 cd WhiteCore-Dev/
 git pull https://github.com/WhiteCoreSim/WhiteCore-Dev.git
 ./runprebuild.sh
