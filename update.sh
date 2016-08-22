026cb0402646c0f7ba76df466451c0fc34c35792
WhiteCoreWebUI <nightbeatz89@gmx.net>
2016-08-22 13:17:10 +0200

    Make a backup before updating
    To Provide the ability to return to a previously working System if something goes wrong.

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 # Check for any updates of Linux
 sudo apt-get update
-# Update WhiteCore-Sim
+# go to /
 cd /
+# Run backup of current Working WC System
+tar czf backup.$(date +%Y%m%d-%H%M%S).tar.gz Github
+# Update WhiteCore-Sim
 cd Github/
 cd WhiteCore-Dev/
 git pull https://github.com/WhiteCoreSim/WhiteCore-Dev.git
